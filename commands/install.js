96fbe98f56532b454ef8fb35229a812d1d68a060
oliver foster <oliver.foster>
2015-05-02 13:44:32 +0100

    Updates

--- commands/install.js
+++ commands/install.js
@@ -47,23 +47,29 @@ logger.log("Using Adapt BuildKit version: "+foundDir,0);
 
 console.log("Adapt BuildKit NPM Install...");	
 var oldCwd = process.cwd();
-process.chdir( path.join(origin, foundDir) );
+process.chdir( path.join(origin, foundDir, "buildkit") );
+console.log(path.join(origin, foundDir));
+process.umask(0000);
 var npm = require("npm");
 npm.load(function(er, npm) {
-	npm.commands.install(function() {
+	npm.commands.install(function(er, data) {
+		if (er) {
+			console.log(er, data);
+			process.exit(1)
+		}
 		console.log("Finished Adapt BuildKit NPM Install.")
 		copyMaker();
 	});			
 });
 
 
+var done = 0;
+
 function copyMaker() {
 	console.log("Installing BuildKit into current directory....")
 	process.chdir(oldCwd);
 	origin = path.join(origin, foundDir)
 	var list = fsext.glob( origin, "**");
-
-	var done = 0;
 
 	for (var i = 0, l = list.length; i < l; i ++) {
 		var item = list[i];
@@ -73,23 +79,56 @@ function copyMaker() {
 		if (item.dir) {
 			fsext.mkdirp({ dest: outputPath });
 			done++;
-			if (done >= list.length) {
-				console.log("Finished installing BuildKit.", done, "files copied.");	
-			}
 		} else {
 			var dirname = path.dirname(outputPath);
 			fsext.mkdirp({ dest: dirname });
 
-			var readStream = fs.createReadStream(item.path)
-
-			readStream.pipe(fs.createWriteStream(outputPath));
-
-			readStream.on("end", function() {
-				done++;
-				if (done >= list.length) {
-					console.log("Finished installing BuildKit.", done, "files copied.");	
-				}
-			});
+			addCopyTask(item.path, outputPath);
 		}
 	}
+}
+
+var copyTasks = [];
+var limit = 20;
+var currentRunning = 0;
+var copyInterval = null;
+
+function addCopyTask(from, to) {
+	copyTasks.push({
+		from: from,
+		to: to
+	});
+	copyStart();
+}
+function copyStart() {
+	if (copyInterval !== null) return;
+	copyInterval = setInterval(runCopyTasks);
+}
+function runCopyTasks() {
+	if (currentRunning >= limit) return;
+	for (var i = 0, l = copyTasks.length; i < l && currentRunning < limit; i++) {
+		var task = copyTasks.shift();
+		copy(task.from, task.to);
+	}
+	
+}
+function copyFinish() {
+	if (copyInterval === null) return;
+	clearInterval(copyInterval);
+}
+
+function copy(from, to) {
+	currentRunning++;
+	var readStream = fs.createReadStream(from)
+
+	readStream.pipe(fs.createWriteStream(to));
+
+	readStream.on("end", function() {
+		done++;
+		currentRunning--;
+		if (copyTasks.length === 0 && currentRunning <= 0) {
+			copyFinish();
+			console.log("Finished installing BuildKit.", done, "files copied.");	
+		}
+	});
 }
