e8c2b85c3b5779e78c654992fd39f4e7aa4cceb1
oliver foster <oliver.foster>
2015-05-07 10:10:17 +0100

    Installer permissions fix

--- scripts/install.js
+++ scripts/install.js
@@ -13,22 +13,10 @@ logger.log("Fixing permission on buildkits...", 0);
 logger.log("Fixing permission on temp folder...", 0);
 
 
-var origin = path.join(__dirname, "../buildkits");
+var origin = path.join(__dirname, "../");
 origin.replace(/\\/g, "/");
 
-var list = fsext.glob( origin, "**");
-
-for (var i = 0, l = list.length; i < l; i ++) {
-	fs.chmodSync(list[i].path, 0777);
-}
-
-
-
-origin = path.join(__dirname, "../temp");
-if (!fs.existsSync(origin)) fsext.mkdirp({dest:origin, norel:true});
-origin.replace(/\\/g, "/");
-
-list = fsext.glob( origin, "**");
+var list = fsext.glob( origin, [ "**/buildkits", "**/temp", "**/buildkits/**", "**/temp/**" ] );
 
 for (var i = 0, l = list.length; i < l; i ++) {
 	fs.chmodSync(list[i].path, 0777);
